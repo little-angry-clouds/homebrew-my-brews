bce0654b12f9e068391dca41c4ebf6de37663636
Àlex Pérez <alexperezpujol@disroot.org>
2020-05-11 00:29:12 +0200

    Initial commit

---
+++ Formula/helmenv.rb
@@ -0,0 +1,18 @@
+class Helmenv < Formula
+  desc "Helm version manager inspired by tfenv"
+  homepage "https://github.com/little-angry-clouds/kubernetes-binaries-managers/"
+  url "https://github.com/little-angry-clouds/kubernetes-binaries-managers/archive/0.0.2.tar.gz"
+  sha256 "92aec08db4dc05c745094b382313025624f843cabc4eb680873bf2fa610727c3"
+
+  depends_on "go" => :build
+
+  def install
+    ENV.prepend "CGO_ENABLED", "0"
+    system "go", "build", "-a", "-o", "helmenv.bin", "helmenv/main.go"
+    bin.install "helmenv.bin" => "#{prefix}/helmenv"
+  end
+
+  test do
+    system "#{bin}/helmenv", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Little-angry-clouds Brew Formaulas
+
+## How do I install these formulae?
+`brew install little-angry-clouds/my-brews/`
+
+Or `brew tap little-angry-clouds/my-brws` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/little-angry-clouds/homebrew-my-brews/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
