27e0c09d185d0b1411e3bec1b12de978f346dfbf
Alex Perez-Pujol <alexperez@paradigmadigital.com>
2020-05-17 13:06:57 +0200

    Update version

--- Formula/helmenv.rb
+++ Formula/helmenv.rb
@@ -1,15 +1,17 @@
 class Helmenv < Formula
   desc "Helm version manager inspired by tfenv"
   homepage "https://github.com/little-angry-clouds/kubernetes-binaries-managers/"
-  url "https://github.com/little-angry-clouds/kubernetes-binaries-managers/archive/0.0.2.tar.gz"
-  sha256 "92aec08db4dc05c745094b382313025624f843cabc4eb680873bf2fa610727c3"
+  url "https://github.com/little-angry-clouds/kubernetes-binaries-managers/archive/0.0.3.tar.gz"
+  sha256 "869b1a710a38a7fb11f52a58511fb4535dcb60911f92752b350cd49ac37b6adf"
 
   depends_on "go" => :build
 
   def install
     ENV.prepend "CGO_ENABLED", "0"
-    system "go", "build", "-a", "-o", "helmenv.bin", "helmenv/main.go"
+    system "go", "build", "-a", "-o", "helmenv.bin", "cmd/helmenv/main.go"
     bin.install "helmenv.bin" => "#{bin}/helmenv"
+    system "go", "build", "-a", "-o", "helm.bin", "cmd/helm-wrapper/main.go"
+    bin.install "helm.bin" => "#{bin}/helm"
   end
 
   test do
--- Formula/kbenv.rb
+++ Formula/kbenv.rb
@@ -1,15 +1,17 @@
 class Kbenv < Formula
   desc "Kubectl version manager inspired by tfenv"
   homepage "https://github.com/little-angry-clouds/kubernetes-binaries-managers/"
-  url "https://github.com/little-angry-clouds/kubernetes-binaries-managers/archive/0.0.2.tar.gz"
-  sha256 "92aec08db4dc05c745094b382313025624f843cabc4eb680873bf2fa610727c3"
+  url "https://github.com/little-angry-clouds/kubernetes-binaries-managers/archive/0.0.3.tar.gz"
+  sha256 "869b1a710a38a7fb11f52a58511fb4535dcb60911f92752b350cd49ac37b6adf"
 
   depends_on "go" => :build
 
   def install
     ENV.prepend "CGO_ENABLED", "0"
-    system "go", "build", "-a", "-o", "kbenv.bin", "kbenv/main.go"
+    system "go", "build", "-a", "-o", "kbenv.bin", "cmd/kbenv/main.go"
     bin.install "kbenv.bin" => "#{bin}/kbenv"
+    system "go", "build", "-a", "-o", "kubectl.bin", "cmd/kubectl-wrapper/main.go"
+    bin.install "kubectl.bin" => "#{bin}/kubectl"
   end
 
   test do
