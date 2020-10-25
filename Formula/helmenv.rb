45f8568ea96e26a22974edf2aab1e8884394bcfe
Àlex Pérez <alexperezpujol@disroot.org>
2020-10-25 17:33:22 +0100

    Update helmenv.rb

--- Formula/helmenv.rb
+++ Formula/helmenv.rb
@@ -1,7 +1,7 @@
 class Helmenv < Formula
   desc "Helm version manager inspired by tfenv"
   homepage "https://github.com/little-angry-clouds/kubernetes-binaries-managers/"
-  url "https://github.com/little-angry-clouds/kubernetes-binaries-managers/archive/0.2.4.tar.gz"
+  url "https://github.com/little-angry-clouds/kubernetes-binaries-managers/archive/0.2.5.tar.gz"
   sha256 "89fc3cbd2e51245bac87302bbedc6c81864df961ae25ca45d41f2ec65930b16d"
 
   depends_on "go" => :build
