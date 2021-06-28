2b0a7013b67c1f6fa57bed061cb8867ab6fc2734
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-29 00:23:50 +0200

    Brew formula update for particle version v0.0.1

--- Formula/particle.rb
+++ Formula/particle.rb
@@ -11,23 +11,23 @@ class Particle < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_Darwin_x86_64.tar.gz"
-      sha256 "8040a6cf92823d75e4af728b411e35271cdcd34b8dfd7326fd84b550b3efac5a"
-    end
-    if Hardware::CPU.arm?
-      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_Darwin_arm64.tar.gz"
-      sha256 "06dac9de21060098ecb6ba3ca62d42f2000a04d492f0a8e72b5a72f87ae33330"
+      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_darwin_amd64.tar.gz"
+      sha256 "55dd74d1dbe5b0543db7a5e85f5160dac47055be9aeee83efa4191cae20da608"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_Linux_x86_64.tar.gz"
-      sha256 "f3bd71f22e50199fb0726395adaf5fcc45209f3051a8b428d4a56d0700246488"
+      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_linux_amd64.tar.gz"
+      sha256 "414159fd6f14caf3525a5699e2607b79c64e20ceb88e72d8a2d534101e7d7f32"
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_linux_armv6.tar.gz"
+      sha256 "e0f38bf6f225c071a1200ca783946cb208a4f6d54319b96a1a935e92cf3e5430"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_Linux_arm64.tar.gz"
-      sha256 "601f0c585dc50f4a956d7c2a1724ba6e34448e8e782d0e9273b152d715b7f716"
+      url "https://github.com/little-angry-clouds/homebrew-my-brewsv0.0.1/particle_0.0.1_linux_arm64.tar.gz"
+      sha256 "0022d4c56833cd890524bda54856adb4dcd753b8668283a407cd2fe7f2c681ff"
     end
   end
 
