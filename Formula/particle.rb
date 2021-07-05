52b3dcb376768b55653543fe0ecd09e6dfa7dc65
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-05 23:53:08 +0200

    Brew formula update for particle version v0.0.3

--- Formula/particle.rb
+++ Formula/particle.rb
@@ -5,29 +5,29 @@
 class Particle < Formula
   desc "Particle is a project designed to aid in the development and testing of Helm charts and other kubernetes manifests."
   homepage "https://github.com/little-angry-clouds/particle/"
-  version "0.0.2"
+  version "0.0.3"
   license "GPL3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.2/particle_0.0.2_darwin_amd64.tar.gz"
-      sha256 "a7ca989f2263cbc9eb8ee41fc9a62748cbf2305edbde85ce6b69a2414ac16871"
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.3/particle_0.0.3_darwin_amd64.tar.gz"
+      sha256 "c5f78319b2673282adb3f5249b9aa3bd38c2040ef572263fc71aad70be9a3510"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.2/particle_0.0.2_linux_amd64.tar.gz"
-      sha256 "5442118be852e40e83bd90b1116e2ea791a6a31b92dba56816f26fa298aa9e56"
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.3/particle_0.0.3_linux_amd64.tar.gz"
+      sha256 "6f3d782e21b8403ee8c62ba095149097a8c2ab9599a720d1ae008908eeef103c"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.2/particle_0.0.2_linux_armv6.tar.gz"
-      sha256 "5c611c56ff54c2934760f85268e82266b0c4495195b0ae258d961909e24e0b25"
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.3/particle_0.0.3_linux_armv6.tar.gz"
+      sha256 "3baefd411cedc5dff049f40cee70e17dcdcbd1483bf0af8e19b6b849a2dfab4a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.2/particle_0.0.2_linux_arm64.tar.gz"
-      sha256 "00106874f9a8357db2b783bd00fbdcd3495ce8af01d58fe7a43e9cde584e9884"
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.3/particle_0.0.3_linux_arm64.tar.gz"
+      sha256 "e7d7460701f663db50be1d09817575716dbeceb402f00286a3e954a0851d383e"
     end
   end
 
