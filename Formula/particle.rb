fc7073bfd98696c7e50343428bed429a3e114d02
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-04 17:10:11 +0100

    Brew formula update for particle version v0.0.9

--- Formula/particle.rb
+++ Formula/particle.rb
@@ -5,37 +5,48 @@
 class Particle < Formula
   desc "Particle is a project designed to aid in the development and testing of Helm charts and other kubernetes manifests."
   homepage "https://github.com/little-angry-clouds/particle/"
-  version "0.0.8"
+  version "0.0.9"
   license "GPL3"
-  bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.8/particle_0.0.8_darwin_amd64.tar.gz"
-      sha256 "3a930819203cde6f917e1b6cf88f38eb75c634210d931e862d077c0d8dcb466a"
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.9/particle_0.0.9_darwin_amd64.tar.gz"
+      sha256 "cda9be2f1643021258b97727e277a168ab77979f0c0b0353b392d9666d75804f"
+
+      def install
+        bin.install "particle"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.8/particle_0.0.8_linux_amd64.tar.gz"
-      sha256 "d554bae85ee6d075573dd4541cb3d2a71a66adf11691c294749abd0e2d01d6f7"
-    end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.8/particle_0.0.8_linux_armv6.tar.gz"
-      sha256 "24f20eee4681041a82b1384920d8a59796f76c5f2fb3d67e93539d561d76633b"
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.9/particle_0.0.9_linux_armv6.tar.gz"
+      sha256 "e2da566cf23fee109712c3f8483022f304f7e93de22b7fba84892b90d3d1008a"
+
+      def install
+        bin.install "particle"
+      end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.8/particle_0.0.8_linux_arm64.tar.gz"
-      sha256 "d36ae1b34cc5385727a3e9f6e36f4be65559e642b7571c6143c044b93b733230"
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.9/particle_0.0.9_linux_arm64.tar.gz"
+      sha256 "1abaadab3e880833cfb958ee3cbad8b4726a98cf7248548493005da3ac8a59b2"
+
+      def install
+        bin.install "particle"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/little-angry-clouds/particle/releases/download/v0.0.9/particle_0.0.9_linux_amd64.tar.gz"
+      sha256 "4fc7090187e4a630af1d646979d9f27e45ef7d484449e106ba907fa205da5b7c"
+
+      def install
+        bin.install "particle"
+      end
     end
   end
 
   depends_on "go"
   depends_on "helm" => :optional
   depends_on "kind" => :optional
-
-  def install
-    bin.install "particle"
-  end
 end
